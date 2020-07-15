036a9a2c345fe673ea95d35fa6b527ba761f598d
sangwan2 <yesdouble@navre.com>
2020-07-15 20:53:14 +0900

    #0.6

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,12 @@
+const yesdouble = {
+  name: "sangwan2",
+  age: 18,
+  gender: "male",
+};
+
 const resolvers = {
   Query: {
-    name: () => "yesdouble",
+    person: () => yesdouble,
   },
 };
 
