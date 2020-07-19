77d402ea131c896fc5d916f93386d062f200f6ee
sangwan2 <yesdouble@navre.com>
2020-07-19 20:06:41 +0900

    #0 add Mutaion

--- graphql/db.js
+++ graphql/db.js
@@ -1,43 +1,49 @@
-export const people = [
+let movies = [
   {
-    id: "0",
-    name: "sangwan1",
-    age: 18,
-    gender: "male",
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1,
   },
   {
-    id: "1",
-    name: "sangwan2",
-    age: 19,
-    gender: "male",
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8,
   },
   {
-    id: "2",
-    name: "sangwan2",
-    age: 20,
-    gender: "male",
+    id: 2,
+    name: "The Godfather I",
+    score: 99,
   },
   {
-    id: "3",
-    name: "sangwan4",
-    age: 21,
-    gender: "male",
-  },
-  {
-    id: "4",
-    name: "sangwan5",
-    age: 22,
-    gender: "male",
-  },
-  {
-    id: "5",
-    name: "sangwan6",
-    age: 23,
-    gender: "male",
+    id: 3,
+    name: "Logan",
+    score: 2,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filterPeople = people.filter((person) => person.id === String(id));
-  return filterPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === String(id));
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== String(id));
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
