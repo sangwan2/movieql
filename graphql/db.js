c878b4f116573c53be6b1f804f94b7dc33bcf4ff
sangwan2 <yesdouble@navre.com>
2020-07-19 20:19:54 +0900

    #11 delete Mutaion

--- graphql/db.js
+++ graphql/db.js
@@ -28,16 +28,6 @@ export const getById = (id) => {
   return filteredMovies[0];
 };
 
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== String(id));
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
 export const addMovie = (name, score) => {
   const newMovie = {
     id: `${movies.length + 1}`,
@@ -46,4 +36,14 @@ export const addMovie = (name, score) => {
   };
   movies.push(newMovie);
   return newMovie;
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
 };
