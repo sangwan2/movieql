57a13b7d695c97a7dffbbb72d0f5ef7d25697378
sangwan2 <yesdouble@navre.com>
2020-07-19 21:00:40 +0900

    #13 Rest API를 GraphQL로 감싸기 파트2

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,8 +2,7 @@ import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id),
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
   },
 };
 
